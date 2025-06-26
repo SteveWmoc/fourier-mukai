a57a5e9a92f22218bf7b3bdfef37607bb86b58aa
SteveWmoc <steves1962wmoc@gmail.com>
2025-06-25 22:55:26 -0500

    Update Basic.lean

--- FourierMukai/Basic.lean
+++ FourierMukai/Basic.lean
@@ -1,5 +1,9 @@
+-- This file provides basic definitions for the Fourier-Mukai formalization
+
 /-
 Copyright (c) 2025 Steven Sabean. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Steven Sabean
 -/
+
+-- Basic definitions for Fourier-Mukai theory
