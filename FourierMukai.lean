66771e1c65ad6e6548379a04d3c4121697ea8870
SteveWmoc <steves1962wmoc@gmail.com>
2025-06-25 19:09:07 -0500

    Delete FourierMukai.lean

--- FourierMukai.lean
+++
@@ -1,8 +0,0 @@
--- This module serves as the root of the `FourierMukai` library.
--- Import modules here that should be built as part of the library.
-import FourierMukai.Basic
-import Mathlib.CategoryTheory.Category.Basic
-
-open CategoryTheory
-
-#check Category
