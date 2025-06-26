ca7ce44f35ba1917eb4ffac206bb34ccffbad3b7
SteveWmoc <steves1962wmoc@gmail.com>
2025-06-25 22:29:17 -0500

    Update Basic.lean

--- FourierMukai/Basic.lean
+++ FourierMukai/Basic.lean
@@ -1,9 +1,5 @@
-import Mathlib.Algebra.Ring.Basic
-import Mathlib.Algebra.Field.Basic
-import Mathlib.Algebra.Category.Ring.Basic
-
--- Define a field k as a global variable for algebraic geometry
-variable (k : Type) [Field k]
-
--- Define a type for commutative rings using Sigma
-def CommRingType := Σ R : Type, CommRing R
+/-
+Copyright (c) 2025 Steven Sabean. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Steven Sabean
+-/
